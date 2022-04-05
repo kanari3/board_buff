ea033faf6f0697533c63d0b3bd2ef8db9f0258c6
kanari3 <kanari333333@gmail.com>
2022-04-05 14:21:13 +0900

    refacotr: ホーム画面を作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:bloc_provider/bloc_provider.dart';
-import 'package:board_buff/ui/page1.dart';
-import 'package:board_buff/model/bloc/page1_bloc.dart';
-import 'package:board_buff/ui/page2.dart';
-import 'package:board_buff/model/bloc/page2_bloc.dart';
+import 'package:board_buff/ui/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,79 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            ElevatedButton(
-              child: const Text("すとりーむさんぷる"),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => BlocProvider(
-                      creator: (context, _) => Page1Bloc(),
-                      child: const Page1(),
-                    ),
-                  ),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: const Text("つうしんさんぷる"),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => BlocProvider(
-                      creator: (context, _) => Page2Bloc(),
-                      child: const Page2(),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
