6fe07e26915799fa5fc1396b4fac1dedc8b66626
kanari3 <kanari333333@gmail.com>
2022-04-23 22:49:03 +0900

    address entiryを追加

---
+++ lib/address.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'address.freezed.dart';
+part 'address.g.dart';
+
+@freezed
+abstract class Address with _$Address {
+  factory Address({
+    @JsonKey(name: 'address1') required String address1,
+    @JsonKey(name: 'address2') required String address2,
+    @JsonKey(name: 'address3') required String address3,
+  }) = _Address;
+  Address._();
+
+  factory Address.fromJson(Map<String, dynamic> json) =>
+      _$AddressFromJson(json);
+}
