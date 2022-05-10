b015a7542af63909a2bee4f234001f8315b32ad2
kanari3 <kanari333333@gmail.com>
2022-05-11 01:57:41 +0900

    feat: article entityを作成

---
+++ lib/model/entity/article/article.dart
@@ -0,0 +1,32 @@
+import 'package:board_buff/model/entity/user/user.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'article.freezed.dart';
+part 'article.g.dart';
+
+@freezed
+abstract class Article with _$Article {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  factory Article({
+    int? id,
+    String? postType,
+    String? title,
+    String? slug,
+    bool? published,
+    int? commentsCount,
+    int? likedCount,
+    int? bodyLetters_count,
+    int? readingTime,
+    String? articleType,
+    String? emoji,
+    bool? isSuspendingPrivate,
+    String? publishedAt,
+    String? bodyUpdatedAt,
+    String? sourceRepoUpdatedAt,
+    User? user,
+    bool? publication,
+  }) = _Article;
+  Article._();
+
+  factory Article.fromJson(Map<String, dynamic> json) => _$ArticleFromJson(json);
+}
